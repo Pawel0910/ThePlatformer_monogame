885315f53e93994c10cf76905d19e1c59be122ec
Pawel <Kielan@gmail.com>
2016-06-11 19:55:21 +0200

    Dodano kamerę

--- ThePlatformer/Camera.cs
+++ ThePlatformer/Camera.cs
@@ -1,1 +1,51 @@
-﻿
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePlatformer
+
+{
+    public class Camera
+    {
+        private Matrix transform;
+        private float Zoom = 1f;
+        private float rotation = 0;
+        public Matrix Transform
+        {
+            get { return transform; }
+        }
+        private Vector2 centre;
+        private Viewport viewport;
+        public Camera(Viewport newViewport)
+        {
+            viewport = newViewport;
+        }
+        public void Update(Vector2 position, int xOffset, int yOffset)
+        {
+            //if (position.X < viewport.Width / 2)
+            //    centre.X = viewport.Width / 2;
+            //else if (position.X > xOffset - (viewport.Width / 2))
+            //{
+            //    centre.X = xOffset - (viewport.Width / 2);
+            //}
+            //else centre.X = position.X;
+
+            //if (position.Y < viewport.Height / 2)
+            //    centre.Y = viewport.Height / 2;
+            //else if (position.Y > yOffset - (viewport.Height / 2))
+            //{
+            //    centre.Y = yOffset - (viewport.Height / 2);
+            //}
+            //else centre.Y = position.Y;
+            centre.X = position.X;
+            centre.Y = position.Y;
+
+            transform = Matrix.CreateTranslation(new Vector3(-centre.X + (viewport.Width / 2),
+                -centre.Y + (viewport.Height / 2), 0)) * Matrix.CreateScale(Zoom, Zoom, 1.0f);
+        }
+    }
+}
