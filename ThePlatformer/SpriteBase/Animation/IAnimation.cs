515316ee4199e73ee3122d78e36ae353669e0b01
Pawel <Kielan@gmail.com>
2016-08-25 22:06:25 +0200

    Dodano interfejs wraz z dokumentacją do Animacji

---
+++ ThePlatformer/SpriteBase/Animation/IAnimation.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace ThePlatformer.SpriteBase.Animation
+{
+    /// <summary>
+    /// Loads all images found by prefix and create a animiation by a specified params.
+    /// </summary>
+    interface IAnimation
+    {
+        void LoadConent(ContentManager content);
+
+        /// <summary>
+        /// Set event on specified frame.
+        /// </summary>
+        /// <param name="textureName">Name of given animation.</param>
+        /// <param name="frame">Frame on which the event is to occur.</param>
+        /// <param name="eventName">Method name which be called.</param>
+        void setEventOnAnimation(String textureName, String eventName, int frame);
+
+        void setCurrentAnimation(String textureName);
+
+        /// <summary>
+        /// Returns null if the frame has not change.
+        /// </summary>
+        /// <returns></returns>
+        Texture2D proceedAnimation(GameTime gameTime);
+    }
+}
