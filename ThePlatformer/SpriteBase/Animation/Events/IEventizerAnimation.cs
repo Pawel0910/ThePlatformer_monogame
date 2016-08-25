eea937010aa2e56150a6337fc42d7b003b38e9c4
Pawel <Kielan@gmail.com>
2016-08-25 22:01:02 +0200

    Dodano interfejs eventów które będzie docelowo mozna dodać na poszczególych klatkach animacji

---
+++ ThePlatformer/SpriteBase/Animation/Events/IEventizerAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePlatformer.SpriteBase.Animation
+{
+    /// <summary>
+    /// This class calls event(method) on specified frame in animation. 
+    /// </summary>
+    interface IEventizerAnimation
+    {
+        /// <summary>
+        /// Starts event on specified frame.
+        /// </summary>
+        /// <param name="currentFrame"></param>
+        void runEvent(int currentFrame);
+    }
+}
