f51df9f87e2e08a688390302fdb213277a31fc50
Pawel <Kielan@gmail.com>
2016-08-29 20:49:20 +0200

    Dodano tło oraz klase która je scrolluje(tło ruchome)

---
+++ ThePlatformer/View/Background/Background.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePlatformer.View.Background
+{
+    public class Background
+    {
+        public Texture2D texture;
+        public Rectangle rect;
+        public void Initialize() { }
+        public void LoadContent(ContentManager Content, String assetName, Rectangle rect)
+        {
+            texture = Content.Load<Texture2D>(assetName);
+            this.rect = rect;
+        }
+        public void Update(GameTime gameTime) { }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, rect, Color.White);
+        }
+    }
+}
---
+++ ThePlatformer/View/Background/Scrolling.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePlatformer.View.Background
+{
+    public class Scrolling : Background
+    {
+        public Scrolling() { }
+        public void Update()
+        {
+            rect.X -= 3;
+        }
+    }
+}
